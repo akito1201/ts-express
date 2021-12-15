ac16d57467c412eef0592230fc4f545dad4e9144
akito1201 <akito.tennis@gmail.com>
2021-12-15 09:49:43 +0900

    TSのimport

---
+++ dist/practice/practice-import.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+//外部モジュールをインポートする時の書き方
+var practice_export_1 = __importDefault(require("./practice-export"));
+console.log(practice_export_1.default);
---
+++ src/practice/practice-import.ts
@@ -0,0 +1,5 @@
+//外部モジュールをインポートする時の書き方
+import practice from './practice-export';
+// こっちでも可
+import practice2 = require('./practice-export');
+console.log(practice);
